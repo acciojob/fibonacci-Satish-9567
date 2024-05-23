d69c2c33fe8982c24642bceed47c31e9f5844248
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-05-23 13:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function fibonacci(num) {
 // your code here
-	if (num==0) {
+	if (num<=1) {
 		return 0;
 	}
-	if (num==1) {
+	if (num==2) {
 		return 1;
 	}
 	return fibonacci(num-2)+fibonacci(num-1);
